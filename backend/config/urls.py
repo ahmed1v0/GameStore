f97c56b8eec9269d7097f20e383bae55ac1d94bb
ahmed1v0 <ahmed.shanableh@digitaltriangle.io>
2026-09-03 22:40:14 +0300

    Add JWT authentication

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-urlpatterns = [path("admin/", admin.site.urls)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/v1/auth/login", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+]
